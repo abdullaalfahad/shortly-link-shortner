a1f9e973fa4033fde6dd24a1cdeed741740a696c
abdullaalfahad <aafahad02@gmail.com>
2022-06-23 15:28:57 +0600

    showcase added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Showcase from './components/Showcase';
 
 export default function App() {
   return (
     <>
       <Header></Header>   
+      <Showcase></Showcase>
     </>
   )
 }
